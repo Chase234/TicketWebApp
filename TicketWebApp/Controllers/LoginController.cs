1bd77b57a9da2886f203099d9eb99b3a2e1a0c77
Chase <luke_chase0@hotmail.co.uk>
2020-02-01 22:37:19 +0000

    Created Login Page

---
+++ TicketWebApp/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicketWebApp.Models;
+
+namespace TicketWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult IndexLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(TicketWebApp.Models.UserLogin userModel)
+        {
+            using (WebTicketsEntities db = new WebTicketsEntities())
+            {
+                var userDetails = db.UserLogins.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessave = "Wrong Username or Password";
+                        return View("IndexLogin", userModel);
+                }
+                else
+                {
+                    Session["UserID"] = userDetails.UserID;
+                }return RedirectToAction("Index", "Home");
+            }
+               
+                return View();
+
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("IndexLogin", "Login");
+        }
+
+    }
+}
